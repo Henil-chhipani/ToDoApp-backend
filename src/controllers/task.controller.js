8fa68049369988176515861fde4a2c098fce035b
Henil Chhipani <henilchhipani@gmail.com>
2025-02-10 16:05:59 +0530

    changes

--- src/controllers/task.controller.js
+++ src/controllers/task.controller.js
@@ -1,4 +1,4 @@
-import { asyncHandler } from "../utils/AsyncHandler.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
 import { apiError } from "../utils/apiError.js";
 import { apiResponse } from "../utils/apiResponse.js";
 import { task } from "../models/task.model.js";
